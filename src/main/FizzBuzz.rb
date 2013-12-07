e95cec4c1acaf0cbabedf2fc8bdf42677549660b
gongmingqm10 <gongmingqm10@foxmail.com>
2013-12-07 11:53:52 +0800

    [gongming] #1 Add unit test

---
+++ src/main/FizzBuzz.rb
@@ -0,0 +1,14 @@
+class FizzBuzz
+
+  def divisible_five(number)
+    # code here
+  end
+
+  def divisible_three(number)
+    # code here
+  end
+
+  def divisible_fifteen(number)
+    # code here
+  end
+end
