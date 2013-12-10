bf30e759786c1ed93fa0778dea14bb5e1d1ee9c4
gongmingqm10 <gongmingqm10@foxmail.com>
2013-12-10 17:32:57 +0800

    gongming: To simplify method

--- src/lib/fizz_buzz.rb
+++ src/lib/fizz_buzz.rb
@@ -1,38 +1,48 @@
 class FizzBuzz
 
-  def divisible_five(number)
-    number%5 == 0
-  end
-
-  def divisible_three(number)
-    number%3 == 0
-  end
-
-  def not_divisible_three_or_five(number)
-     !(divisible_three(number)||divisible_five(number))
-  end
-
-  def exchange(numbers)
+  def to_fizzbuzz(numbers)
     numbers = numbers.squeeze(' ').split
-    result = Array.new
+    result = []
     numbers.each do |number|
       if number.match(/^\d+$/)
         number = number.to_i
-        result.push("Fizz") if divisible_three(number)
-        result.push("Buzz") if divisible_five(number)
-        result.push(number) if not_divisible_three_or_five(number)
+        if fizzbuzz_number? number
+          result.push("FizzBuzz")
+          next
+        end
+        result.push("Fizz") if fizz_number?(number)
+        result.push('Buzz') if buzz_number?(number)
+        result.push(number) if original_number?(number)
       else
         return 'ERROR'
       end
     end
-    return result.join(' ')
+    result
+  end
+
+  private
+
+  def buzz_number?(number)
+    number%5 == 0
+  end
+
+  def fizz_number?(number)
+    number%3 == 0
+  end
+
+  def fizzbuzz_number?(number)
+    buzz_number?(number)&&fizz_number?(number)
+  end
+
+  def original_number?(number)
+    !(buzz_number?(number)||fizz_number?(number))
   end
 end
 
 if __FILE__ == $0
   fizz_buzz_new = FizzBuzz.new
   print 'Input all the numbers:'
-  array = gets
-  result = fizz_buzz_new.exchange array
-  puts result
+  str = gets
+  result = fizz_buzz_new.to_fizzbuzz str
+  puts result.join(' ')
 end
--- src/spec/fizz_buzz_spec.rb
+++ src/spec/fizz_buzz_spec.rb
@@ -3,45 +3,21 @@ require 'fizz_buzz'
 describe FizzBuzz do
   subject { FizzBuzz.new }
 
-  it 'should be true when 20 divided by 5' do
-    actual = subject.divisible_five 20
-    actual.should be_true
-  end
+  describe 'exchange function test' do
+    context 'normal input' do
+      it 'should be "Fizz Fizz Buzz Buzz Fizz Buzz" when fizzbuzzed as "3 0 5 15 13"' do
+        actual = subject.to_fizzbuzz '3 0 5 15 13'
+        expected = ['Fizz','FizzBuzz','Buzz','FizzBuzz',13]
+        expect(actual).to eq(expected)
+      end
+    end
 
-  it 'should be true when 18 divided by 3' do
-    actual = subject.divisible_three 18
-    actual.should be_true
-  end
-
-  it 'should be false when 18 divided by 5' do
-    actual = subject.divisible_five 18
-    actual.should be_false
-  end
-
-  it 'should be false when 20 divided by 3' do
-    actual = subject.divisible_three 20
-    actual.should be_false
-  end
-
-  it 'should be true when 11 divided by neither 3 nor 5' do
-    actual = subject.not_divisible_three_or_five 11
-    actual.should be_true
-  end
-
-  it 'should be false when 12 divided by neither 3 nor 5' do
-  actual = subject.not_divisible_three_or_five 12
-  actual.should be_false
-  end
-
-  it 'should be "Fizz Fizz Buzz Buzz Fizz Buzz" when explained as "3,0,5,15"' do
-    actual = subject.exchange '3 0 5 15'
-    expected = "Fizz Fizz Buzz Buzz Fizz Buzz"
-    expect(actual).to eq(expected)
-  end
-
-  it 'should get a "ERROR" when input some non number' do
-    actual = subject.exchange '3 ds 8   '
-    expect(actual).to eq('ERROR')
+    context 'error input' do
+      it 'should get a "ERROR" when input some non number' do
+        actual = subject.to_fizzbuzz '3 ds 8   '
+        expect(actual).to eq('ERROR')
+      end
+    end
   end
 
 end
