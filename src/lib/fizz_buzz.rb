c40214ba9a50b195c03230f7014844424c3d03f7
gongmingqm10 <gongmingqm10@foxmail.com>
2013-12-12 13:07:33 +0800

    add Fizz number function

--- src/lib/fizz_buzz.rb
+++ src/lib/fizz_buzz.rb
@@ -1,5 +1,14 @@
 class FizzBuzz
 
+  def compile_numbers number
+    'Fizz' if fizz_number? number
+  end
 
+
+  private
+
+  def fizz_number? number
+    number%3 ==0
+  end
 
 end
--- src/spec/fizz_buzz_spec.rb
+++ src/spec/fizz_buzz_spec.rb
@@ -3,6 +3,13 @@ require 'fizz_buzz'
 describe FizzBuzz do
   subject { FizzBuzz.new }
 
+  describe 'test compile_numbers function' do
+    it 'should return Fizz when input 3' do
+      actual = subject.compile_numbers 3
+      expect(actual).to eq('Fizz')
+    end
+  end
+
 
 
 
