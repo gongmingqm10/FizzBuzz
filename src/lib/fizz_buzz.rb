f7fe13d7e459557a7e0422ad70a1dbae0a376d9f
gongmingqm10 <gongmingqm10@foxmail.com>
2013-12-12 08:04:19 +0800

    gongming: change to code to initial state

--- src/lib/fizz_buzz.rb
+++ src/lib/fizz_buzz.rb
@@ -1,48 +1,5 @@
 class FizzBuzz
 
-  def to_fizzbuzz(numbers)
-    numbers = numbers.squeeze(' ').split
-    result = []
-    numbers.each do |number|
-      if number.match(/^\d+$/)
-        number = number.to_i
-        if fizzbuzz_number? number
-          result.push("FizzBuzz")
-          next
-        end
-        result.push("Fizz") if fizz_number?(number)
-        result.push('Buzz') if buzz_number?(number)
-        result.push(number) if original_number?(number)
-      else
-        return 'ERROR'
-      end
-    end
-    result
-  end
 
-  private
 
-  def buzz_number?(number)
-    number%5 == 0
-  end
-
-  def fizz_number?(number)
-    number%3 == 0
-  end
-
-  def fizzbuzz_number?(number)
-    buzz_number?(number)&&fizz_number?(number)
-  end
-
-  def original_number?(number)
-    !(buzz_number?(number)||fizz_number?(number))
-  end
-end
-
-if __FILE__ == $0
-  fizz_buzz_new = FizzBuzz.new
-  print 'Input all the numbers:'
-  str = gets
-  result = fizz_buzz_new.to_fizzbuzz str
-  puts result.join(' ')
 end
--- src/spec/fizz_buzz_spec.rb
+++ src/spec/fizz_buzz_spec.rb
@@ -3,21 +3,7 @@ require 'fizz_buzz'
 describe FizzBuzz do
   subject { FizzBuzz.new }
 
-  describe 'exchange function test' do
-    context 'normal input' do
-      it 'should be "Fizz Fizz Buzz Buzz Fizz Buzz" when fizzbuzzed as "3 0 5 15 13"' do
-        actual = subject.to_fizzbuzz '3 0 5 15 13'
-        expected = ['Fizz','FizzBuzz','Buzz','FizzBuzz',13]
-        expect(actual).to eq(expected)
-      end
-    end
 
-    context 'error input' do
-      it 'should get a "ERROR" when input some non number' do
-        actual = subject.to_fizzbuzz '3 ds 8   '
-        expect(actual).to eq('ERROR')
-      end
-    end
-  end
+
 
 end
