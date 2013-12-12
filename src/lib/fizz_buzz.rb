4753afd808681277b16e98944ff8832f8e5c5e9b
gongmingqm10 <gongmingqm10@foxmail.com>
2013-12-12 13:46:50 +0800

    gongming: test function with 3, 5, and 15

--- src/lib/fizz_buzz.rb
+++ src/lib/fizz_buzz.rb
@@ -2,6 +2,8 @@ class FizzBuzz
 
   def compile_numbers number
     'Fizz' if fizz_number? number
+    'Buzz' if buzz_number? number
+    'FizzBuzz' if fizzbuzz_number? number
   end
 
 
@@ -9,6 +11,14 @@ class FizzBuzz
 
   def fizz_number? number
     number%3 ==0
+  end
+
+  def buzz_number? number
+    number%5 == 0
+  end
+
+  def fizzbuzz_number? number
+    number%15==0
   end
 
 end
--- src/spec/fizz_buzz_spec.rb
+++ src/spec/fizz_buzz_spec.rb
@@ -6,7 +6,17 @@ describe FizzBuzz do
   describe 'test compile_numbers function' do
     it 'should return Fizz when input 3' do
       actual = subject.compile_numbers 3
-      expect(actual).to eq('Fizz')
+      actual == 'Fizz'
+    end
+
+    it 'should return Buzz when input 5' do
+      actual = subject.compile_numbers 5
+      actual == 'Buzz'
+    end
+
+    it 'should return FizzBuzz when input 15' do
+      actual = subject.compile_numbers 15
+      actual == 'FizzBuzz'
     end
   end
 
